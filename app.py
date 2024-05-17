94975d4f32e88ae461b733384022ecfbc3551624
Aniniel <123729884+Aniniel@users.noreply.github.com>
2024-05-17 11:02:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def search():
         return render_template('results.html', results=None, search_type=search_type, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
